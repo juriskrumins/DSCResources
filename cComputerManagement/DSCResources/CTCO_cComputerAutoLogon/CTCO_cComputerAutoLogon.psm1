966cd9a643f5c7b8229bd699556b9e318dc9eb06
Juris Krumins <juris.krumins80@gmail.com>
2014-10-16 17:50:47 +0300

    Multiple new resource.
    - cComputerAutoLogon resource
    - cClusterAvailableDisk resource
    - cClusterQuorum resource

---
+++ cComputerManagement/DSCResources/CTCO_cComputerAutoLogon/CTCO_cComputerAutoLogon.psm1
@@ -0,0 +1,121 @@
+function Get-TargetResource
+{
+    [OutputType([Hashtable])]
+    param (
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultUserName,
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultDomainName,
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultPassword,
+      [Parameter(Mandatory=$false)]
+      [Int]$AutoLogonCount = 1,
+      [Parameter(Mandatory=$false)]
+      [Boolean]$Reboot=$false
+     )
+
+    $returnValue = @{}
+    try
+    {
+        $ErrorActionPreference = "Stop"
+        Write-Verbose "Getting required registry values"
+        Set-Location "HKLM:\Software\Microsoft\Windows NT\Currentversion\WinLogon"
+        $returnValue = @{
+            AutoAdminLogon = Get-ItemProperty -Path $pwd.Path -Name "AutoAdminLogon" -ErrorAction SilentlyContinue
+            DefaultUserName = Get-ItemProperty -Path $pwd.Path -Name "DefaultUserName" -ErrorAction SilentlyContinue
+            DefaultPassword = Get-ItemProperty -Path $pwd.Path -Name "DefaultPassword" -ErrorAction SilentlyContinue
+            DefaultDomainName = Get-ItemProperty -Path $pwd.Path -Name "DefaultDomainName" -ErrorAction SilentlyContinue
+            AutoLogonCount = Get-ItemProperty -Path $pwd.Path -Name "AutoLogonCount" -ErrorAction SilentlyContinue
+            Reboot = $Reboot
+        }
+    }
+    catch 
+    {
+        Write-Verbose -Message "Error occured. Error $($Error[0].Exception.Message)"
+    }
+	$returnValue
+}
+
+
+function Set-TargetResource
+{
+    param (
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultUserName,
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultDomainName,
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultPassword,
+      [Parameter(Mandatory=$false)]
+      [Int]$AutoLogonCount = 1,
+      [Parameter(Mandatory=$false)]
+      [Boolean]$Reboot=$false
+     )
+
+    try
+    {
+        $ErrorActionPreference = "Stop"
+        Write-Verbose "Adding required registry values."
+        Set-Location "HKLM:\Software\Microsoft\Windows NT\Currentversion\WinLogon"
+        New-ItemProperty -Path $pwd.Path -Name "AutoAdminLogon" -Value 1 -PropertyType "String" -Force
+        New-ItemProperty -Path $pwd.Path -Name "DefaultUserName" -Value $DefaultUserName -PropertyType "String" -Force
+        New-ItemProperty -Path $pwd.Path -Name "DefaultPassword" -Value $DefaultPassword -PropertyType "String" -Force
+        New-ItemProperty -Path $pwd.Path -Name "DefaultDomainName" -Value $DefaultDomainName -PropertyType "String" -Force
+        New-ItemProperty -Path $pwd.Path -Name "AutoLogonCount" -Value $AutoLogonCount -PropertyType "Dword" -Force
+        New-ItemProperty -Path $pwd.Path -Name "AutoLogonSetByDSC" -Value 1 -PropertyType "String" -Force
+        if($Reboot)
+        {
+            Write-Verbose -Message "AutoLogon registry values have been set."
+            Write-Verbose -Message "Indicating to LCM that system needs reboot."
+            $global:DSCMachineStatus = 1
+        }
+    }
+    catch 
+    {
+        Write-Verbose -Message "Error occured. Error $($Error[0].Exception.Message)"
+    }
+
+}
+
+
+function Test-TargetResource
+{
+    [OutputType([Boolean])]
+    param (
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultUserName,
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultDomainName,
+      [Parameter(Mandatory=$true)]
+      [String]$DefaultPassword,
+      [Parameter(Mandatory=$false)]
+      [Int]$AutoLogonCount = 1,
+      [Parameter(Mandatory=$false)]
+      [Boolean]$Reboot=$false
+     )
+    $retValue = $false
+    Write-Verbose "Getting required registry values"
+    Set-Location "HKLM:\Software\Microsoft\Windows NT\Currentversion\WinLogon"
+    $AutoLogonSetByDSC = Get-ItemProperty -Path $pwd.Path -Name "AutoLogonSetByDSC" -ErrorAction SilentlyContinue
+    $AutoLogonStatus = Get-TargetResource  -DefaultUserName $DefaultUserName -DefaultDomainName $DefaultDomainName -DefaultPassword $DefaultPassword -AutoLogonCount $AutoLogonCount -Reboot $Reboot
+    if($AutoLogonSetByDSC.AutoLogonSetByDSC -eq "1")
+    {
+        if(($AutoLogonStatus["DefaultUserName"].DefaultUserName -eq $DefaultUserName) -and ($AutoLogonStatus["DefaultDomainName"].DefaultDomainName -eq $DefaultDomainName))
+        {
+            Write-Verbose "AutoLogon registry values have been set by DSC and looks correct."
+            $retValue = $true
+        }
+        elseif(($AutoLogonStatus["DefaultUserName"].DefaultUserName -ne $DefaultUserName) -or ($AutoLogonStatus["DefaultDomainName"].DefaultDomainName -ne $DefaultDomainName))
+        {
+            Write-Verbose "AutoLogon registry values have been set by DSC, but current values are incorrect. Going to set them up once again."
+        }
+    }
+    else
+    {
+        Write-Verbose "AutoLogon registry values have not been set by DSC. Going to setup proper registry values."
+    }
+    return $retValue
+}
+
+
+Export-ModuleMember -Function *-TargetResource
---
+++ cFailOverCluster/DSCResources/CTCO_cClusterQuorum/CTCO_cClusterQuorum.psm1
@@ -0,0 +1,233 @@
+﻿function Get-TargetResource
+{
+    [OutputType([Hashtable])]
+    param
+    (	
+        [parameter(Mandatory)]
+        [string] $ClusterName,
+
+        [parameter(Mandatory)]
+        [ValidateSet("DiskOnly", "NodeAndDiskMajority","NodeAndFileShareMajority","NodeMajority")]
+        [string] $QuorumType,
+
+        [parameter()]
+        [string] $QuorumResource=$null,
+        
+        [parameter(Mandatory)]
+        [PSCredential] $DomainAdministratorCredential
+    )
+
+
+    $retValue=@{}  
+    try
+    {
+        $ErrorActionPreference = "Stop"  
+        ($oldToken, $context, $newToken) = ImpersonateAs -cred $DomainAdministratorCredential
+        $cluster = Get-Cluster -Name $ClusterName
+        if ($null -eq $cluster)
+        {
+            throw "Can't find the cluster $ClusterName"
+        }
+
+        $quorum = $cluster | Get-ClusterQuorum
+        $retValue = @{
+            ClusterName = $quorum.Cluster
+            QuorumResource = $quorum.QuorumResource
+            QuorumType = $quorum.QuorumType
+            DomainAdministratorCredential = $DomainAdministratorCredential
+        }
+    }
+    finally
+    {
+        if ($context)
+        {
+            $context.Undo()
+            $context.Dispose()
+            CloseUserToken($newToken)
+        }
+    }
+
+    return $retValue
+}
+
+function Set-TargetResource
+{
+    param
+    (	
+        [parameter(Mandatory)]
+        [string] $ClusterName,
+
+        [parameter(Mandatory)]
+        [ValidateSet("DiskOnly", "NodeAndDiskMajority","NodeAndFileShareMajority","NodeMajority")]
+        [string] $QuorumType,
+
+        [parameter()]
+        [string] $QuorumResource=$null,
+        
+        [parameter(Mandatory)]
+        [PSCredential] $DomainAdministratorCredential
+    )
+
+ 
+    try
+    {
+        ($oldToken, $context, $newToken) = ImpersonateAs -cred $DomainAdministratorCredential
+        Write-Verbose -Message "Getting cluster $ClusterName ..."
+        $cluster = Get-Cluster -Name $ClusterName
+        if ($null -eq $cluster)
+        {
+            throw "Can't find the cluster $ClusterName"
+        }
+        $parameters = @{}
+        if($QuorumType -eq "NodeMajority")
+        {
+            Write-Verbose -Message "QuorumType  is $QuorumType"
+            $parameters += @{NodeMajority=$null}
+        }
+        elseif(($QuorumType -ne "NodeMajority") -and ($QuorumResource -ne $null))
+        {
+            Write-Verbose -Message "QuorumType  is $QuorumType"
+            Write-Verbose -Message "QuorumResource is $QuorumResource"
+            $parameters += @{"$QuorumType"="$QuorumResource"}
+        }
+        elseif(($QuorumType -ne "NodeMajority") -and ($QuorumResource -eq $null))
+        {
+            Write-Verbose -Message "QuorumResource parameter should be specified for QuorumType other than NodeMajority"
+            Write-Verbose -Message "Thus we'll skip Set function."
+        }
+
+        if($parameters.Count -ne 0){
+            Write-Verbose -Message "Try to setup cluster quorum for cluster $ClusterName ..."
+            $cluster | Set-ClusterQuorum @parameters
+        }
+        else
+        {
+            Write-Verbose -Message "Set-ClusterQuorum cmdlet parameter list is empty."
+        }
+    }
+    finally
+    {
+        if ($context)
+        {
+            $context.Undo()
+            $context.Dispose()
+            CloseUserToken($newToken)
+        }
+    }
+}
+
+
+function Test-TargetResource  
+{
+    [OutputType([Boolean])]
+    param
+    (	
+        [parameter(Mandatory)]
+        [string] $ClusterName,
+
+        [parameter(Mandatory)]
+        [ValidateSet("DiskOnly", "NodeAndDiskMajority","NodeAndFileShareMajority","NodeMajority")]
+        [string] $QuorumType,
+
+        [parameter()]
+        [string] $QuorumResource=$null,
+        
+        [parameter(Mandatory)]
+        [PSCredential] $DomainAdministratorCredential
+    )
+
+    $retValue = $false
+
+    try
+    {
+        $ErrorActionPreference = "Stop"
+	    ($oldToken, $context, $newToken) = ImpersonateAs -cred $DomainAdministratorCredential
+        Write-Verbose -Message "Getting cluster $Name quorum info ..."
+        $quorum = Get-TargetResource -ClusterName $ClusterName -QuorumType $QuorumType -QuorumResource $QuorumResource -DomainAdministratorCredential $DomainAdministratorCredential
+        if($quorum.Count -eq 0)
+        {
+            Write-Verbose -Message "Can't get cluster quorum info."
+        }
+        elseif(($quorum.Count -ne 0) -and ($quorum["QuorumType"] -like $QuorumType) -and ($quorum["QuorumResource"] -like $QuorumResource))
+        {
+            Write-Verbose -Message "Cluster quorum configuration is good."
+            $retValue = $true
+        }
+        elseif(($quorum.Count -ne 0) -and (($quorum["QuorumType"] -notlike $QuorumType) -or ($quorum["QuorumResource"] -notlike $QuorumResource)))
+        {
+            Write-Verbose -Message "Cluster quorum type and/or quorum resource is incorrect."
+        }
+        else
+        {
+            Write-Verbose -Message "Something strage happened."
+        }
+    }
+    finally
+    {    
+	    if ($context)
+	    {
+		    $context.Undo()
+		    $context.Dispose()
+
+		    CloseUserToken($newToken)
+	    }
+    }
+    return $retValue
+}
+
+
+#region Additional functions
+
+function Get-ImpersonatetLib
+{
+    if ($script:ImpersonateLib)
+    {
+        return $script:ImpersonateLib
+    }
+
+    $sig = @'
+[DllImport("advapi32.dll", SetLastError = true)]
+public static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+
+[DllImport("kernel32.dll")]
+public static extern Boolean CloseHandle(IntPtr hObject);
+'@ 
+   $script:ImpersonateLib = Add-Type -PassThru -Namespace 'Lib.Impersonation' -Name ImpersonationLib -MemberDefinition $sig 
+
+   return $script:ImpersonateLib
+    
+}
+
+function ImpersonateAs([PSCredential] $cred)
+{
+    [IntPtr] $userToken = [Security.Principal.WindowsIdentity]::GetCurrent().Token
+    $userToken
+    $ImpersonateLib = Get-ImpersonatetLib
+
+    $bLogin = $ImpersonateLib::LogonUser($cred.GetNetworkCredential().UserName, $cred.GetNetworkCredential().Domain, $cred.GetNetworkCredential().Password, 
+    9, 0, [ref]$userToken)
+    
+    if ($bLogin)
+    {
+        $Identity = New-Object Security.Principal.WindowsIdentity $userToken
+        $context = $Identity.Impersonate()
+    }
+    else
+    {
+        throw "Can't Logon as User $cred.GetNetworkCredential().UserName."
+    }
+    $context, $userToken
+}
+
+function CloseUserToken([IntPtr] $token)
+{
+    $ImpersonateLib = Get-ImpersonatetLib
+
+    $bLogin = $ImpersonateLib::CloseHandle($token)
+    if (!$bLogin)
+    {
+        throw "Can't close token"
+    }
+}
+
+#endregion Additional function
